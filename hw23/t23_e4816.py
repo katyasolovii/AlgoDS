c5ef580f6c25cf126dba3d65456e9a76074768b7
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2025-05-07 23:28:10 +0300

    added e4816

---
+++ hw23/t23_e4816.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+def solve(n, edges):
+    graph = [[]] * (n + 1)
+    for u, v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+
+    visited = [0] * (n + 1)
+    components = []
+    
+    for i in range(1, n + 1):
+        if visited[i] == 0:
+            queue = deque()
+            queue.append(i)
+            visited[i] = 1
+            component = []
+            
+            while queue:
+                u = queue.popleft()
+                component.append(u)
+                for v in graph[u]:
+                    if visited[v] == 0:
+                        visited[v] = 1
+                        queue.append(v)
+            components.append(component)
+    print(len(components))
+    for j in components:
+        print(len(j))
+        print(*j)
+
+
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        lines = f.readlines()
+        n, m = map(int, lines[0].split())
+        edges = []
+        for i in range(1, m + 1):
+            u, v = map(int, lines[i].split())
+            edges.append((u, v))
+    solve(n, edges)
